a5a5f1484f1b9a8a5e07f6fab0f4938df257b3c5
yanapvl <yanapvlnko@gmail.com>
2025-10-29 14:13:42 +0200

    оновлення структури проекту

--- LitShare.DAL/obj/Debug/net8.0/LitShare.DAL.AssemblyInfo.cs
+++ LitShare.DAL/obj/Debug/net8.0/LitShare.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LitShare.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0cfdd03125af71b7d6ab270b5a15eca9424b0ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LitShare.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LitShare.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
