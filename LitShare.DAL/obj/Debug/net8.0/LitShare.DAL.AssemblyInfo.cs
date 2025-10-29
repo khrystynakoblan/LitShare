a0cfdd03125af71b7d6ab270b5a15eca9424b0ed
yanapvl <yanapvlnko@gmail.com>
2025-10-29 10:07:15 +0200

    edited the files

--- LitShare.DAL/obj/Debug/net8.0/LitShare.DAL.AssemblyInfo.cs
+++ LitShare.DAL/obj/Debug/net8.0/LitShare.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LitShare.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd5aabbd3cca3cdb575215e64abaca0632c1b661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LitShare.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LitShare.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
