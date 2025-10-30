40eac1788c65bcd149ff40c2e83c3c29d13a1403
yanapvl <yanapvlnko@gmail.com>
2025-10-30 16:13:37 +0200

    changed the project architecture

---
+++ LitShare.Presentation/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using LitShare.BLL.Services; // <-- Ось де потрібні 'using'
+
+namespace LitShare.Presentation
+{
+    public partial class MainWindow : Window
+    {
+        private readonly UserService _userService = new UserService();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var allUsers = _userService.GetAllUsers();
+                usersGrid.ItemsSource = allUsers;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"ПОМИЛКА ЗАВАНТАЖЕННЯ: {ex.Message}",
+                                "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
--- LitShare.Presentation/obj/Debug/net8.0-windows/LitShare.Presentation.AssemblyInfo.cs
+++ LitShare.Presentation/obj/Debug/net8.0-windows/LitShare.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LitShare.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b616583d243d89c953b3501f2fbd26a37dafc5cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LitShare.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LitShare.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
