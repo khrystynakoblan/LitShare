74c01e5ee8e440552539d7f643ecf3b5101d02ff
Vlada <havvlada@gmail.com>
2025-10-31 13:46:30 +0200

    Updated EditProfileWindow and ProfileViewWindow

--- LitShare.Presentation/ProfileViewWindow.xaml.cs
+++ LitShare.Presentation/ProfileViewWindow.xaml.cs
@@ -9,9 +9,22 @@ namespace LitShare.Presentation
             InitializeComponent();
         }
 
+        
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Перехід на головну сторінку (ще не реалізовано).");
+        }
+
+        
+        private void MyProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Перехід у мій профіль (редагування).");
+        }
+
+        
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            MessageBox.Show("Повернення назад на попередню сторінку.");
         }
     }
 }
